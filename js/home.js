6dc6e5d6ba55075627fcf18cd05294ed435ea432
abdullah628 <abdullah628360@gmail.com>
2024-09-24 08:50:46 +0600

    Blog page created and added functionality

--- js/home.js
+++ js/home.js
@@ -1,43 +1,38 @@
 let totalAmount = parseFloat(document.getElementById("total-amount").innerText);
 
-
-
-let quotaFund = parseFloat(document.getElementById("quota-fund").innerText);
-
-
-
-function addHistory(donationName, amount){
-    const div = document.createElement("div");
-    const date = new Date();
-    div.innerHTML = `
+function addHistory(donationName, amount) {
+  const div = document.createElement("div");
+  const date = new Date();
+  div.innerHTML = `
         <div class="border p-6 rounded-md mb-4">
           <h1 class="text-md font-bold mb-2">
             ${amount} ${donationName}
           </h1>
           <p class="text-sm text-[#808080]">${date.toString()}</p>
         </div>
-    `
+    `;
 
-    document.getElementById("history-part").appendChild(div);
+  document.getElementById("history-part").appendChild(div);
 }
-
-
-    
-
 
 document
   .getElementById("noakhali-donation-btn")
   .addEventListener("click", function () {
-    let noakhaliFund = parseFloat(document.getElementById("noakhali-fund").innerText);
-    const noakhaliDonation = parseFloat(document.getElementById("noakhali-donation").value);
-    const nokhaliDonationName = document.getElementById("noakhali-donation-name").innerText;
-    
-    if(isNaN(noakhaliDonation) || noakhaliDonation < 0) {
-        alert("Invalid input !!");
-        return;
+    let noakhaliFund = parseFloat(
+      document.getElementById("noakhali-fund").innerText
+    );
+    const noakhaliDonation = parseFloat(
+      document.getElementById("noakhali-donation").value
+    );
+    const nokhaliDonationName = document.getElementById(
+      "noakhali-donation-name"
+    ).innerText;
+
+    if (isNaN(noakhaliDonation) || noakhaliDonation < 0) {
+      alert("Invalid input !!");
+      return;
     }
 
-    
     noakhaliFund += noakhaliDonation;
     totalAmount -= noakhaliDonation;
 
@@ -48,19 +43,21 @@ document
     document.getElementById("openModal").click();
   });
 
-
-  document
+document
   .getElementById("feni-donation-btn")
   .addEventListener("click", function () {
     let feniFund = parseFloat(document.getElementById("feni-fund").innerText);
-    const feniDonation = parseFloat(document.getElementById("feni-donation").value);
-    const feniDonationName = document.getElementById("feni-donation-name").innerText;
-    
-    if(isNaN(feniDonation) || feniDonation < 0) {
-        alert("Invalid input !!");
-        return;
+    const feniDonation = parseFloat(
+      document.getElementById("feni-donation").value
+    );
+    const feniDonationName =
+      document.getElementById("feni-donation-name").innerText;
+
+    if (isNaN(feniDonation) || feniDonation < 0) {
+      alert("Invalid input !!");
+      return;
     }
-    
+
     feniFund += feniDonation;
     totalAmount -= feniDonation;
 
@@ -71,19 +68,22 @@ document
     document.getElementById("openModal").click();
   });
 
-
-  document
+document
   .getElementById("quota-donation-btn")
-  .addEventListener("click", function () {
+  .addEventListener("click", function (e) {
     let quotaFund = parseFloat(document.getElementById("quota-fund").innerText);
-    const quotaDonation = parseFloat(document.getElementById("quota-donation").value);
-    const quotaDonationName = document.getElementById("quota-donation-name").innerText;
-    
-    if(isNaN(quotaDonation) || quotaDonation < 0) {
-        alert("Invalid input !!");
-        return;
+    const quotaDonation = parseFloat(
+      document.getElementById("quota-donation").value
+    );
+    const quotaDonationName = document.getElementById(
+      "quota-donation-name"
+    ).innerText;
+
+    if (isNaN(quotaDonation) || quotaDonation < 0) {
+      alert("Invalid input !!");
+      return;
     }
-    
+
     quotaFund += quotaDonation;
     totalAmount -= quotaDonation;
 
@@ -91,5 +91,10 @@ document
     document.getElementById("total-amount").innerText = totalAmount;
 
     addHistory(quotaDonationName, quotaDonation);
+    e.preventDefault();
     document.getElementById("openModal").click();
+  });
+
+  document.getElementById("blog-btn").addEventListener("click", function () {
+    window.location.href = "/blog.html";
   });
